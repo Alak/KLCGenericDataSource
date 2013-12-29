fd28b7072971e4e021975e96a9f82ca8c268f38b
Kevin CATHALY <alakme@gmail.com>
2013-12-29 12:24:08 +0100

    modify git ignore

---
+++ GenericDataSource/KLCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KLCTableViewController.h
+//  GenericDataSource
+//
+//  Created by Kevin CATHALY on 29/12/2013.
+//  Copyright (c) 2013 Kevin Laurent CATHALY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KLCTableViewController : UITableViewController
+
+@end
---
+++ GenericDataSourceLibrary/KLCGenericDataSource.h
@@ -0,0 +1,14 @@
+//
+//  KLCGenericDataSource.h
+//  
+//
+//  Created by Kevin CATHALY on 29/12/2013.
+//
+//
+
+#ifndef _KLCGenericDataSource_h
+#define _KLCGenericDataSource_h
+
+
+
+#endif
